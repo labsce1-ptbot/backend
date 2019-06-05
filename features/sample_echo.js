87024c1a38ef2b3041012cac2c7ceaa681b40c86
Ta1grr <taile602@gmail.com>
2019-06-05 13:24:42 -0500

    Finally got the mention endpoint working

--- features/sample_echo.js
+++ features/sample_echo.js
@@ -9,8 +9,30 @@ module.exports = function(controller) {
         await bot.reply(message, 'I heard a sample message.');
     });
 
-    controller.on('message', async(bot, message) => {
-        await bot.reply(message, `Echo: ${ message.text }`);
-    });
+    // const user = { id: 'UK7L9AYFR' };
+    // const user = { id: 'UJUDJ1VH8' };
+    // const user = 'taile602';
+    const user = {
+        '<@UK7L9AYFR>' : true,
+    }
+    const users = "<@UK7L9AYFR>"
+
+    // controller.hears(`${user}`, 'message,direct_message', async(bot, message) => {
+    //     console.log(message)
+    //     await bot.reply(message, `${user} is on vacation`);
+    // });
+
+    controller.on('message', async (bot, message) => {
+        const compare = message.incoming_message.channelData.text
+        if (user[compare]) {
+            await bot.reply(message, `Hey <@${message.incoming_message.channelData.user}>, ${message.incoming_message.channelData.text} is currently on Vacation`)
+        }
+        // console.log(message)
+        // await bot.reply(message, `${message.text}`);
+    })
+
+    // controller.on('message', async(bot, message) => {
+    //     await bot.reply(message, `Echo: ${ message.text }`);
+    // });
 
 }
