a95b802efefc245039198d12229185537deecfb1
Ta1grr <taile602@gmail.com>
2019-06-05 14:04:39 -0500

    cleaned up and added in comments

--- features/sample_echo.js
+++ features/sample_echo.js
@@ -9,28 +9,26 @@ module.exports = function(controller) {
         await bot.reply(message, 'I heard a sample message.');
     });
 
-    // const user = { id: 'UK7L9AYFR' };
-    // const user = { id: 'UJUDJ1VH8' };
-    // const user = 'taile602';
     const user = {
         '<@UK7L9AYFR>' : true,
     }
     const users = "<@UK7L9AYFR>"
 
+    //Hard coded, but doesn't work as well
     // controller.hears(`${user}`, 'message,direct_message', async(bot, message) => {
     //     console.log(message)
     //     await bot.reply(message, `${user} is on vacation`);
     // });
 
+    //Listen for users and make comparison on user object
     controller.on('message', async (bot, message) => {
         const compare = message.incoming_message.channelData.text
         if (user[compare]) {
             await bot.reply(message, `Hey <@${message.incoming_message.channelData.user}>, ${message.incoming_message.channelData.text} is currently on Vacation`)
         }
-        // console.log(message)
-        // await bot.reply(message, `${message.text}`);
     })
 
+    // Sample_echo
     // controller.on('message', async(bot, message) => {
     //     await bot.reply(message, `Echo: ${ message.text }`);
     // });
