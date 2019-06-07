d02ff23677ff7c7edbca1781172265bd179d276a
Ta1grr <taile602@gmail.com>
2019-06-06 21:55:08 -0500

    Changed the overall layout, added in express for our server and botkit

---
+++ controllers/features/sample_echo.js
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+module.exports = function(controller) {
+
+    controller.hears('sample','message', async(bot, message) => {
+        await bot.reply(message, 'I heard a sample message.');
+    });
+
+    const user = {
+        '<@UK7L9AYFR>' : true,
+    }
+    const users = "<@UK7L9AYFR>"
+
+    //Hard coded, but doesn't work as well
+    // controller.hears(`${user}`, 'message,direct_message', async(bot, message) => {
+    //     console.log(message)
+    //     await bot.reply(message, `${user} is on vacation`);
+    // });
+
+    //Listen for users and make comparison on user object
+    controller.on('message', async (bot, message) => {
+        const compare = message.incoming_message.channelData.text
+        if (user[compare]) {
+            await bot.replyInThread(message, `Hey <@${message.incoming_message.channelData.user}>, ${message.incoming_message.channelData.text} is currently on Vacation`)
+        }
+    })
+
+    // Sample_echo
+    // controller.on('message', async(bot, message) => {
+    //     await bot.reply(message, `Echo: ${ message.text }`);
+    // });
+
+}
---
+++ routers/botkitRouter.js
@@ -0,0 +1,12 @@
+const slack = require('../controllers/bot.js')
+const express = require("express");
+const router = express.Router();
+
+module.exports = function(app) {
+    router.post('/', function(req, res) {
+        res.status(200);
+
+        slack.controller.handleWebhookPayload(req, res)
+    })
+
+}
