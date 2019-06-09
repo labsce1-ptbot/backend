fa5d1ab1fa2c26346a185a50d813e2fd5646dbaf
Brian G <brian.gregorius@gmail.com>
2019-06-09 16:25:49 -0400

    added moments to package.json and added a hears function

--- controllers/features/sample_echo.js
+++ controllers/features/sample_echo.js
@@ -2,35 +2,52 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License.
  */
-
+let mongoDB = require("../../config/db");
+let add_date = require("../../routers/routers");
+// const express = require("express");
+// const app = express();
+// const Article = require("../../models/event-model");
 module.exports = function(controller) {
+  controller.hears("sample", "message", async (bot, message) => {
+    await bot.reply(message, "I heard a sample message.");
+  });
 
-    controller.hears('sample','message', async(bot, message) => {
-        await bot.reply(message, 'I heard a sample message.');
-    });
+  const user = {
+    "<@UK7L9AYFR>": true
+  };
+  const users = "<@UK7L9AYFR>";
 
-    const user = {
-        '<@UK7L9AYFR>' : true,
+  //Hard coded, but doesn't work as well
+  // controller.hears(`${user}`, 'message,direct_message', async(bot, message) => {
+  //     console.log(message)
+  //     await bot.reply(message, `${user} is on vacation`);
+  // });
+
+  //Listen for users and make comparison on user object
+  controller.on("message", async (bot, message) => {
+    const compare = message.incoming_message.channelData.text;
+    if (user[compare]) {
+      await bot.replyInThread(
+        message,
+        `Hey <@${message.incoming_message.channelData.user}>, ${
+          message.incoming_message.channelData.text
+        } is currently on Vacation`
+      );
     }
-    const users = "<@UK7L9AYFR>"
+  });
 
-    //Hard coded, but doesn't work as well
-    // controller.hears(`${user}`, 'message,direct_message', async(bot, message) => {
-    //     console.log(message)
-    //     await bot.reply(message, `${user} is on vacation`);
-    // });
+  // Sample_echo
+  // controller.on('message', async(bot, message) => {
+  //     await bot.reply(message, `Echo: ${ message.text }`);
+  // });
 
-    //Listen for users and make comparison on user object
-    controller.on('message', async (bot, message) => {
-        const compare = message.incoming_message.channelData.text
-        if (user[compare]) {
-            await bot.replyInThread(message, `Hey <@${message.incoming_message.channelData.user}>, ${message.incoming_message.channelData.text} is currently on Vacation`)
-        }
-    })
+  controller.hears("yes", "message", async (bot, message) => {
+    // await bot.reply(message, "hi!");
 
-    // Sample_echo
-    // controller.on('message', async(bot, message) => {
-    //     await bot.reply(message, `Echo: ${ message.text }`);
-    // });
+    const yes = await add_date(message);
 
-}
+    // if (add_date(message) == "success") {
+    //   await bot.reply(message, "vacation time scheduled!");
+    // }
+  });
+};
