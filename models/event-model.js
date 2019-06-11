0ca219f9edcb4bb9e14bb3d9d457933ff47587db
Brian G <brian.gregorius@gmail.com>
2019-06-11 11:24:11 -0400

    added find route and deleted eventID

--- models/event-model.js
+++ models/event-model.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const eventSchema = Schema({
-  eventId: {
-    type: String,
-    required: false,
-    unique: true
-  },
+  // eventId: {
+  //   type: String,
+  //   required: false,
+  //   unique: true
+  // },
   slackID: {
     type: String,
     required: true
--- routers/routers.js
+++ routers/routers.js
@@ -5,16 +5,32 @@ async function add_date(message) {
   console.log("<----Date NOW---->\n", Date.now());
   event = new Event();
   // (event.eventId = message.client_msg_id),
-    (event.eventId = "testingthisout"),
+  (event.eventId = "testingthisout"),
     (event.slackID = message.userID),
     (event.startDate = message.start_date),
     (event.endDate = message.end_date),
     (event.message = "message.text");
 
   console.log("<-----EVENT------>", event);
+
+  x = await searchDestroy(event);
+
+  console.log("sdfkjdaklfjakl xxxx", x);
   const dbResponse = await event.save();
 
   return dbResponse;
+}
+
+async function searchDestroy(event) {
+  event.find({
+    slackID: event.userID,
+    $or: [
+      {
+        startDate: { $gte: event.startDate, $lte: event.endDate },
+        endDate: { $gte: event.startDate, $lte: event.endDate }
+      }
+    ]
+  });
 }
 
 module.exports = add_date;
