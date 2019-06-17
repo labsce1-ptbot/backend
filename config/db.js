451cb4d01ea528858a1453106b34f3f6fd33f126
Randy Calderon <randycweb@gmail.com>
2019-06-17 12:01:21 -0700

    Merge conflict fix

--- config/db.js
+++ config/db.js
@@ -1,48 +1,48 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// require("dotenv").config();
-// const MongoClient = require("mongodb").MongoClient;
-// const url = process.env.MONGO_URI;
-// const dbName = "ptbot"
-// const db = mongoose.connection
-// const seeder = require("mongoose-seed")
+require("dotenv").config();
+const MongoClient = require("mongodb").MongoClient;
+const url = process.env.MONGO_URI;
+const dbName = "ptbot"
+const db = mongoose.connection
+const seeder = require("mongoose-seed")
 
 
 // Remember to remove later
-// mongoose.connect(url)
+mongoose.connect(url)
 
 // CREATE DATABASE
-// var data = [
-//   {
-//   model: "User",
-//   documents: [
-//     {username: "john", password: "test"},
-//     {username: "alice", password: "test"},
-//     {username: "frank", password: "test"},
-//     {username: "greeny", password: "test"},
-//     {username: "spottyottydopalicious", password: "test"}
-//   ]
-//  }
-// ]
+var data = [
+  {
+  model: "User",
+  documents: [
+    {username: "john", password: "test"},
+    {username: "alice", password: "test"},
+    {username: "frank", password: "test"},
+    {username: "greeny", password: "test"},
+    {username: "spottyottydopalicious", password: "test"}
+  ]
+ }
+]
 
 // Seed
-  // seeder.connect(url, () => {
-  //   seeder.loadModels(['../models/user-model.js'])
+seeder.connect(url, () => {
+    seeder.loadModels(['../models/user-model.js'])
   
-  //   seeder.clearModels(['User'], () => {
-  //     seeder.populateModels(data, () => {
-  //       seeder.disconnect()
-  //     })
-  //   })
-  // })
+    seeder.clearModels(['User'], () => {
+      seeder.populateModels(data, () => {
+        seeder.disconnect()
+      })
+    })
+  })
 
-// MongoClient.connect(url, function(err, client) {
-//   if (err) throw err;
+MongoClient.connect(url, function(err, client) {
+  if (err) throw err;
   
-//   let dbo = client.db(dbName)
+  let dbo = client.db(dbName)
   
-//   client.close();
-//   console.log("Database created!");
-// });
+  client.close();
+  console.log("Database created!");
+});
 
 
