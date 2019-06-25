6ef24079d142070ecc22f297e9d4fef812e01022
Randy Calderon <randycweb@gmail.com>
2019-06-25 14:18:59 -0700

    Added slack-auth to plugins and set up endpoints for slack

---
+++ controllers/plugins/slack-auth.js
@@ -0,0 +1,43 @@
+const passport = require('passport')
+const request = require('request')
+
+module.exports = (botkit) => {
+  return {
+    // Plugin Name
+    name: 'slack-auth',
+    // Initialize module
+    init: (controller) => {
+      function slackAuthRoutes(req, res, next) {
+        if(req.isAuthenticated()) {
+        controller.webserver.get("/slack/user",(req, res) => {
+          let url = `https://slack.com/oauth/authorize?client_id=${process.env.clientId}&scope=identity.basic`
+          request.get(url,(err, httpResponse, body) => {
+            res.send(body)
+          })
+        })
+
+        controller.webserver.get("/slack/code",  (req, res) => {
+          let code = req.query.code
+          let url = `https://slack.com/api/oauth.access?client_id=${process.env.clientId}&client_secret=${process.env.clientSecret}&code=${code}`
+          request.get(url, (err, httpResponse, body) => {
+            let data = JSON.parse(body)
+            console.log("|---Slack Response----|\n", data)
+            console.log(req.session)
+          })
+          res.send("Got user data, exchange for token now")
+        })
+        controller.webserver.get('/slack/logout', (req, res) => {
+          req.logout();
+          res.redirect('/');
+        });
+      } else {
+        return res.status(401).json({message: "You need to be logged in"})
+      }
+        console.log('REQ:', req.url)
+
+        next();
+      }
+      controller.webserver.use('/slack', slackAuthRoutes);
+    }
+  }
+}
