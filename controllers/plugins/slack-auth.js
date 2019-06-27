4eb4d03d1ad04dec41c25a416360abb85112a46c
Randy Calderon <randycweb@gmail.com>
2019-06-27 08:25:29 -0700

    Pushing new slack info

--- controllers/plugins/slack-auth.js
+++ controllers/plugins/slack-auth.js
@@ -1,6 +1,7 @@
 const passport = require('passport')
 const request = require('request')
-const { ensureAuthenticated } = require("../../config/auth0mw")
+const Slack = require('../../routers/routers')
+const User = require('../../models/user-model')
 
 module.exports = (botkit) => {
   return {
@@ -31,11 +32,15 @@ module.exports = (botkit) => {
             let data = JSON.parse(body)
             console.log("|---Slack Response----|\n", data)
             console.log(req.session)
-            user.access_token = data.access_token
-            console.log("|---Updated Req User with Token---|", user)
+
+            // Figure out how to manage token. Store in database or not?
+            req.session.passport.user.access_token = data.access_token
+            Slack.slackInfo(data)
+            console.log(`|---Updated Req User with Token---|`, user)
+            req.session.save((err) => {console.log("|---Error on session save---|", err)})
+            res.end()
           })
           res.send("Got user data, exchange for token now")
-      
         })
 
         controller.webserver.get('/slack/logout', (req, res) => {
@@ -46,6 +51,17 @@ module.exports = (botkit) => {
         
         next();
         } 
+
+        // Find User for Populate Endpoint change to events in future
+        controller.webserver.get('/slack/info', async (req, res) => {
+          console.log('|--- Slack-info Endpoint---|\n', req.user)
+          const findUser = await User.findOne({
+            email: req.user[0].email,
+          }).populate('slack').exec((err, info) => {
+            console.log("Slack Info:", info)
+          })
+        })
+
         controller.webserver.use('/slack', slackAuthRoutes);
       }
     }
