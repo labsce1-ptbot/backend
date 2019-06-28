6195e6276adf1ad87e3a43164d36350e38af9632
Ta1grr <taile602@gmail.com>
2019-06-28 15:00:23 -0500

    replaced local routes with .env

--- controllers/plugins/passport-oauth.js
+++ controllers/plugins/passport-oauth.js
@@ -43,7 +43,8 @@ module.exports = function(botkit) {
         controller.webserver.get(
           "/auth/callback",
           passport.authenticate("auth0", {
-            successRedirect: "http://localhost:3001/admin/dashboard",
+            successRedirect: `${process.env.REACT_APP_SERVER_URL}`,
+            // "http://localhost:3001/admin/dashboard",
             failureRedirect: "/"
           })
         );
@@ -55,7 +56,7 @@ module.exports = function(botkit) {
             //   failureRedirect: "/auth/login"
             // })
             function(req, res) {
-              res.redirect("/admin/dashboard");
+              res.redirect("/");
             }
           })
         );
