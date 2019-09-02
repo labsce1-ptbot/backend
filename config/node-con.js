cba9a956e05bb38c7fe2acf27ce0e86f3381f18b
Brian G <brian.gregorius@gmail.com>
2019-09-01 21:11:10 -0400

    changed location of node cron

--- config/node-con.js
+++ config/node-con.js
@@ -88,3 +88,22 @@ cron.schedule(
     timezone: "America/New_York"
   }
 );
+
+cron.schedule("30 00 * * *", async () => {
+  console.log("-------------------------\n");
+  console.log("Collecting data from database and saving to cache.js\n");
+  const database = await db.get_date();
+  database.forEach(obj => {
+    (cache[obj.slackID] = {
+      start_date: obj.startDate,
+      end_date: obj.endDate,
+      message: obj.message[0],
+      vacation: true
+    }),
+      {
+        scheduled: true
+      };
+  });
+  // console.log("------------------------\n");
+  // console.log("End results of what's in cache.js:", cache);
+});
